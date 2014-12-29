b51ea473f7d005a1f47f5a278bb173261975cdb1
Ronni Elken Lindsgaard <ronni.lindsgaard@gmail.com>
2014-12-29 01:32:16 +0100

    Implemented post_file endpoint

--- hushfile.py
+++ hushfile.py
@@ -1,11 +1,66 @@
-from flask import Flask
+from base64 import b64encode
+from flask import Flask, request, jsonify
+import json
 import os
+import tempfile
+from werkzeug.exceptions import BadRequest
 
 app = Flask(__name__)
 
 abspath = os.path.abspath(os.path.basename(__file__))
 DATA_PATH = os.environ.get('HUSHFILE_DATA_PATH',
                            os.path.join(abspath, 'files'))
+
+DEFAULT_FILE_EXPIRATION = os.environ.get('HUSHFILE_DEFAULT_FILE_EXPIRATION',
+                                         60*60*24*30)  # 30 days
+FILEID_LENGTH = 15
+
+metadata_filename = 'metadata.json'
+properties_filename = 'properties.json'
+
+
+def generate_password(length=40):
+    return b64encode(os.urandom(42))[:40]
+
+
+def create_new_file(filepath):
+    filepath = tempfile.mkdtemp(dir=DATA_PATH)
+
+    fileid = os.path.basename(filepath)
+
+    app.logger.info("Creating new upload at %s" % filepath)
+
+    return fileid, filepath
+
+
+def read_metadata_file(filepath):
+    with open(os.path.join(filepath, metadata_filename), 'rb') as f:
+        content = f.read()
+    return json.loads(content)
+
+
+def write_metadata_file(filepath, metadata):
+    metadata_file = os.path.join(filepath, metadata_filename)
+
+    with open(metadata_file, 'wb') as f:
+        f.write(json.dumps(metadata))
+
+
+def read_properties_file(filepath):
+    with open(os.path.join(filepath, properties_filename), 'r') as f:
+        content = f.read()
+    return json.loads(content)
+
+
+def write_properties_file(filepath, properties):
+    properties_file = os.path.join(filepath, properties_filename)
+
+    with open(properties_file, 'w') as f:
+        f.write(json.dumps(properties))
+
+
+def parse_request_data(request):
+    return request.get_json() or request.form
 
 
 def not_implemented():
@@ -14,7 +69,35 @@ def not_implemented():
 
 @app.route('/api/file', methods=['POST'])
 def post_file():
-    return not_implemented()
+    payload = parse_request_data(request)
+
+    if not payload['metadata']:
+        app.logger.error("Parsing of request failed")
+        raise BadRequest()
+
+    fileid, filepath = create_new_file(DATA_PATH)
+
+    write_metadata_file(filepath,
+                        {
+                            'metadata': payload['metadata'],
+                            'mac': payload['mac']
+                        })
+
+    uploadpassword = generate_password()
+    properties = {
+        'uploadpassword': uploadpassword,
+        'deletepassword': payload.get('deletepassword', ''),
+        'expire': payload.get('expire', DEFAULT_FILE_EXPIRATION),
+        'limit': payload.get('limit', None),
+        'chunks': payload.get('chunks', None),
+    }
+
+    write_properties_file(filepath, properties)
+
+    return jsonify({
+        'id': fileid,
+        'uploadpassword': uploadpassword,
+    })
 
 
 def assert_file_exists(id):
--- test_hushfile.py
+++ test_hushfile.py
@@ -1,7 +1,15 @@
+from base64 import b64encode
+import json
 import main
+from mock import patch
+import os
 import shutil
 import tempfile
 import unittest
+
+json_header = {
+    'Content-type': 'application/json',
+}
 
 
 class TestHushfile(unittest.TestCase):
@@ -15,5 +23,36 @@ class TestHushfile(unittest.TestCase):
     def tearDown(self):
         shutil.rmtree(self.tmp_dir)
 
-    def test_success(self):
-        pass
+    def api_upload_success(self, wrap_func=lambda x: x, headers={}):
+        payload = {
+            'metadata': b64encode(os.urandom(42)),
+            'mac': '12345'
+        }
+
+        with patch('main.generate_password') as p:
+            with patch('main.DATA_PATH', self.tmp_dir):
+                p.return_value = '1234567890'
+
+                resp = self.app.post('/api/file',
+                                     data=wrap_func(payload),
+                                     headers=headers)
+                self.assertEqual(resp.status_code, 200)
+                data = json.loads(resp.get_data())
+
+                assert os.path.isdir(os.path.join(self.tmp_dir,
+                                                  data['id']))
+
+                # TODO: Test file contents
+
+    def test_api_upload_json(self):
+        self.api_upload_success(wrap_func=json.dumps, headers=json_header)
+
+    def test_api_upload_multipart(self):
+        self.api_upload_success()
+
+    def test_api_upload_no_meta(self):
+        payload = {
+            'mac': '1234'
+        }
+        resp = self.app.post('/api/file', data=payload)
+        self.assertEqual(resp.status_code, 400)
