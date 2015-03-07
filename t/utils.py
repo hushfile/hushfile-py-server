8aedf3d6b20ebc727e39be91fb5ee66567da0c21
Ronni Elken Lindsgaard <rel@zx.dk>
2015-03-07 20:48:13 +0100

    Added a couple of utility functions

--- t/utils.py
+++ t/utils.py
@@ -1,6 +1,21 @@
 from __future__ import division, print_function, unicode_literals
+
+import json
 from nose.tools import eq_ as assert_equal
 
 
 def status_code(resp, expect):
     assert_equal(resp.status_code, expect)
+
+
+def expect_response(resp, status=200, payload=None):
+    status_code(resp, status)
+
+    if payload:
+        assert_equal(
+            json.loads(resp.get_data()),
+            payload)
+
+
+def expect_not_found(resp):
+    status_code(resp, 404)
