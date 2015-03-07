4c35e2c89903174f0051ce83ba2f5ea6b624b517
Ronni Elken Lindsgaard <rel@zx.dk>
2015-03-07 20:19:37 +0100

    Removed function from utils

--- t/utils.py
+++ t/utils.py
@@ -4,13 +4,3 @@ from nose.tools import eq_ as assert_equal
 
 def status_code(resp, expect):
     assert_equal(resp.status_code, expect)
-
-
-def setup_mockfile(file_id, uploadpassword='1234'):
-    filepath = os.path.join(app.tmpdir, file_id)
-    os.makedirs(filepath)
-    with open(os.path.join(filepath, 'properties.json'), 'w') as f:
-        json.dump({
-            'uploadpassword': uploadpassword
-        }, f)
-    return filepath
